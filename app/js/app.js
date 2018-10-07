c038e0b153e8730466d02a0bfa46a0453c4644eb
Asaad Budayr <asad_289@hotmail.com>
2018-10-07 07:47:28 -0700

    Feat: add a button to get a new quote

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,16 @@
 (function() {
-  const quoteContainer = document.querySelector('.quote-container');
+  const quoteBox = document.querySelector('.quote-box');
+  //new quote button
+  const changeQuote = document.querySelector('.newQuote');
+  //make an API request from quotesondesign.com to get random quotes
+  function requestQuote() {
   const quoteRequest = new XMLHttpRequest();
   quoteRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
   quoteRequest.onload = addQuote;
   quoteRequest.onerror = errorFun;
   quoteRequest.send();
+  }
+  requestQuote();
 
   function addQuote() {
     let htmlContent;
@@ -16,11 +22,19 @@
     } else {
       htmlContent = '<div class="error-no-image">No images available</div>';
     }
-    quoteContainer.insertAdjacentHTML('beforeend', htmlContent);
+    quoteBox.insertAdjacentHTML('afterbegin', htmlContent);
   }
 
   function errorFun() {
-    alert('Sorry, we are unable to load quotes');
+    alert('Sorry, we are unable to load quote');
   }
+  //event listener to get a new quote
+  document.addEventListener('click', () => {
+    //remove previous quote
+    while (quoteBox.hasChildNodes()) {
+    quoteBox.removeChild(quoteBox.firstChild);
+    }
+    requestQuote();
+  });
 
 })();

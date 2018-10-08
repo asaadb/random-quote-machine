b43f6b0fd42b526fa821792eb3f1d8a95c3f23a5
Asaad Budayr <asad_289@hotmail.com>
2018-10-08 05:32:20 -0700

    Fix: changed to jQuery synx

--- app/js/app.js
+++ app/js/app.js
@@ -1,45 +1,47 @@
-(function() {
-  const quoteBox = document.querySelector('.quote-box');
+$(document).ready(() => {
+  const quoteBox = $('.quote-box');
   //new quote button
-  const changeQuote = document.querySelector('.newQuote');
+  const changeQuote = $('.newQuote');
   //make an API request from quotesondesign.com to get random quotes
   function requestQuote() {
     $.ajax({
-            url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
-        }).done(addQuote)
-        .fail(errorFun);
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+      }).done(addQuote)
+      .fail(errorFun);
   }
   requestQuote();
 
   function addQuote(data) {
     if (data && data[0]) {
       htmlContent = `${data[0].content}<span id="author">${data[0].title}</span>`;
+      $('.quote-box p').prepend('<i class="fas fa-quote-left"></i>');
     } else {
       htmlContent = '<p class="error-no-image">No quotes available. Try again</p>';
     }
-    quoteBox.insertAdjacentHTML('afterbegin', htmlContent);
+    quoteBox.prepend(htmlContent);
   }
 
   function errorFun() {
     alert('Sorry, we are unable to load quote');
   }
   //event listener to get a new quote
-  changeQuote.addEventListener('click', () => {
+  changeQuote.click(() => {
     //remove previous quote
-    while (quoteBox.hasChildNodes()) {
-    quoteBox.removeChild(quoteBox.firstChild);
-    }
+
+    quoteBox.empty();
+
     //get new quote
     requestQuote();
-    //get tweet button and add an event listener
-    const tweetButton = document.getElementById('tweet');
-    tweetButton.addEventListener('click', quoteTweet);
-    //get the quote and tweet it
-     function quoteTweet(){
-       const twQuote = document.querySelector(".quote-box p").textContent;
-       const twAuthor = document.getElementById("author").textContent;
-       window.open(`https://twitter.com/intent/tweet?text="${twQuote}" ${twAuthor}&hashtags=Inspiring`);
-    }
   });
+  //get tweet button and add an event listener
+  const tweetButton = $('#tweet');
+  tweetButton.click(quoteTweet);
+  //get the quote and tweet it
+  function quoteTweet() {
+    const twQuote = $(".quote-box p").text();
+    const twAuthor = $("author").text();
+    window.open(`https://twitter.com/intent/tweet?text="${twQuote}" ${twAuthor}&hashtags=Inspiring`);
+  }
 
-})();
+
+});
